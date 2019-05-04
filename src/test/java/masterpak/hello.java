46b09e6578e0f384a930ee2f108c73d894bca147
Yoganandhini23 <yoganandhini23@gmail.com>
2019-05-04 22:12:22 +0100

    2nd local commit

---
+++ src/test/java/masterpak/hello.java
@@ -0,0 +1,26 @@
+package masterpak;
+
+import org.testng.annotations.Test;
+
+public class hello {
+
+
+    @Test
+    public void testMethod1(){
+
+        System.out.println("This is @Test1");
+    }
+
+    @Test
+    public void testMethod2(){
+
+        System.out.println("This is @Test2");
+    }
+    @Test
+    public void testMethod3(){
+
+        System.out.println("This is @Test3");
+    }
+
+
+}
