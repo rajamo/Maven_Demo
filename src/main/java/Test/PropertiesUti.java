503fbe1745f8c643c121d4c1a67c4ab8c6e5b82a
Yoganandhini23 <yoganandhini23@gmail.com>
2019-05-05 08:48:47 +0100

    4nd local commit

---
+++ src/main/java/Test/PropertiesUti.java
@@ -0,0 +1,33 @@
+package Test;
+
+import jdk.nashorn.internal.ir.CatchNode;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUti
+{
+
+    public String proper()
+  {
+      try
+      {
+          Properties pro = new Properties();
+          String root = System.getProperty("user.dir");
+          FileInputStream ip = new FileInputStream(root+"/TestData/Config.properties");
+          pro.load(ip);
+          return pro.getProperty("Browser");
+      }
+      catch (Exception exp)
+      {
+          exp.printStackTrace();
+          return null;
+
+      }
+
+
+
+    }
+
+}
