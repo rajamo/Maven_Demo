93417f6f59c4d3e2cdb2883791073599bf024b08
Yoganandhini23 <yoganandhini23@gmail.com>
2019-05-11 20:38:13 +0100

    6th local commit

--- src/test/java/masterpak/browserTest.java
+++ src/test/java/masterpak/browserTest.java
@@ -9,13 +9,24 @@ public class browserTest {
 
 
     @Test
-    @Parameters({"URL"})
-    public void browserOpen(String url){
+    //@Parameters({"URL"})
+    public void browserOpen1(){
         String root = System.getProperty("user.dir");
         System. setProperty("webdriver.chrome.driver", root +"\\Driver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-       // driver.get("https://mail.google.com/mail/u/0/#inbox");
-        driver.get(url);
+       driver.get("https://mail.google.com/mail/u/0/#inbox");
+
+        driver.quit();
+    }
+
+    @Test
+    //@Parameters({"URL"})
+    public void browserOpen2(){
+        String root = System.getProperty("user.dir");
+        System. setProperty("webdriver.chrome.driver", root +"\\Driver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
         driver.quit();
     }
 }
